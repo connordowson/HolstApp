1b11782a73a2972730925af60ff13c4379396f2d
Connor Dowson <connormwdowson@gmail.com>
2018-05-04 14:57:48 +0100

    Changed tour data

--- src/pages/walking-tour/tourdata.ts
+++ src/pages/walking-tour/tourdata.ts
@@ -1,16 +1,36 @@
 export var tourInfo = [
 
     {
-      name: "Rotunda Place",
-      lat: 51.892226,
-      lng: -2.086077
+      name: "16 Rotunda Terrace",
+      lat: 51.895261,
+      lng: -2.082719
     },
 
     {
-      name: "second place",
-      lat: 50,
-      lng: 50
+      name: "The Rotunda",
+      lat: 51.894968,
+      lng: -2.082890
+    },
 
-    }
+    {
+      name: "Queens Circus",
+      lat: 51.896661,
+      lng: -2.080892
+    },
+
+    {
+      name: "Holst Statue",
+      lat: 51.897015,
+      lng: -2.078967
+    },
+
+    {
+      name: "Cheltenham Town Hall",
+      lat: 51.897248,
+      lng: -2.077762
+    },
     
+    
+    
+
 ];
--- www/build/main.js
+++ www/build/main.js
@@ -438,15 +438,30 @@ var AppModule = /** @class */ (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return tourInfo; });
 var tourInfo = [
     {
-        name: "Rotunda Place",
-        lat: 51.892226,
-        lng: -2.086077
+        name: "16 Rotunda Terrace",
+        lat: 51.895261,
+        lng: -2.082719
     },
     {
-        name: "second place",
-        lat: 50,
-        lng: 50
-    }
+        name: "The Rotunda",
+        lat: 51.894968,
+        lng: -2.082890
+    },
+    {
+        name: "Queens Circus",
+        lat: 51.896661,
+        lng: -2.080892
+    },
+    {
+        name: "Holst Statue",
+        lat: 51.897015,
+        lng: -2.078967
+    },
+    {
+        name: "Cheltenham Town Hall",
+        lat: 51.897248,
+        lng: -2.077762
+    },
 ];
 //# sourceMappingURL=tourdata.js.map
 
